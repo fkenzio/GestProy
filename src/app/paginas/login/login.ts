8c3875877b6501c2c7369ccf3df94790f11cee41
Victor Ruiz <victorjoseruizsoto4@gmail.com>
2026-02-06 23:21:22 -0700

    Comenzando

---
+++ src/app/paginas/login/login.ts
@@ -0,0 +1,40 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-login',
+    imports: [FormsModule],
+    templateUrl: './login.html',
+    styleUrl: './login.css'
+})
+export class LoginComponent {
+
+    correo = signal('');
+    password = signal('');
+    cargando = signal(false);
+    error = signal('');
+
+    constructor(private router: Router) { }
+
+    iniciarSesion(): void {
+        this.error.set('');
+
+        if (!this.correo() || !this.password()) {
+            this.error.set('Ingresa correo y contraseña');
+            return;
+        }
+
+        this.cargando.set(true);
+        setTimeout(() => {
+            if (this.correo().includes('@')) {
+                localStorage.setItem('token', 'token-temporal');
+                this.router.navigate(['/dashboard']);
+            } else {
+                this.error.set('Correo o contraseña incorrectos');
+            }
+
+            this.cargando.set(false);
+        }, 1000);
+    }
+}
