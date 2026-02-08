810a62fb0360c32fc9cc98e5e1a65ca38700cee0
Victor Ruiz <victorjoseruizsoto4@gmail.com>
2026-02-08 14:16:02 -0700

    dashboard implementado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './compartidos/guard/auth.guard';
 
 export const routes: Routes = [
     { path: 'login', loadComponent: () => import('./paginas/login/login').then(m => m.LoginComponent) },
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./paginas/dashboard/dashboard').then(m => m.DashboardComponent),
+        canActivate: [authGuard]
+    },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: '**', redirectTo: 'login' }
 ];
